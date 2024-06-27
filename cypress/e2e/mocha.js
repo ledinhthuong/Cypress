bde65e77b16ec55408b4612746653233b01119a2
Thuong Le <thuong.le@limeade.com>
2024-06-27 16:33:06 +0700

    first

---
+++ cypress/e2e/mocha.js
@@ -0,0 +1,25 @@
+describe('suit function', function () {
+    before(function () {
+      // runs once before the first test in this block
+    });
+  
+    after(function () {
+      // runs once after the last test in this block
+    });
+  
+    beforeEach(function () {
+      // runs before each test in this block
+    });
+  
+    afterEach(function () {
+      // runs after each test in this block
+    });
+
+    it('displays two todo items by default', () => { });
+     // Test Case
+})
+
+
+
+
+
